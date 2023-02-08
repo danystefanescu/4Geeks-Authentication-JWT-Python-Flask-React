88b16fcd344c04ef13690e82766e8f1b564ef988
danystefanescu <danydavidgt@gmail.com>
2023-02-08 11:46:35 +0000

    Authentification with JWT finished

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,15 +4,25 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
-
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
+@api.route('/login', methods=['POST'])
+def user_login():
+    body_email = request.json.get("email")
+    body_password = request.json.get("password")
+    user = User.query.filter_by(email= body_email, password= body_password).first()
+    if not user:
+        return jsonify({"error": "Wrong credentials"}), 401
+    token = create_access_token(identity = user.id)
+    return jsonify({"response": "Successfully logged in", "token": token}), 200
+    
+@api.route('/user', methods=['GET'])
+@jwt_required()
+def current_user_email():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+    return jsonify({"response": "Successfully logged in", "email": user.email}), 200
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,64 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const navigate = useNavigate();
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  const sendLogin = async () => {
+    const response = await fetch(
+      "https://3001-4geeksacade-reactflaskh-y7dho6eaapi.ws-eu85.gitpod.io/api/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      }
+    );
+    if (response.ok) {
+      const data = await response.JSON();
+      localStorage.setItem("token", data.token);
+      navigate("/demo");
+    } else {
+      setError(true);
+    }
+  };
+
+  return (
+    <div className="log-form">
+      <h2>Login to your account</h2>
+      <form>
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => {
+            setError(false);
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" className="btn" onClick={() => sendLogin()}>
+          Login
+        </button>
+        {error ? (
+          <p className="alert text-danger">Error en credenciales</p>
+        ) : null}
+      </form>
+    </div>
+  );
 };
